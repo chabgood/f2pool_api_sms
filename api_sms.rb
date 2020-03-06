c7f991e47d27fb05591e4acdff5205ea49f31bfd
Chris Habgood <chabgood@gmail.com>
2020-03-06 11:12:01 -0600

    oop, added coinmarket cap to translate into usd dollars

--- api_sms.rb
+++ api_sms.rb
@@ -17,15 +17,58 @@ require 'httparty'
 require 'action_view'
 require 'twilio-ruby'
 
-include ActionView::Helpers::NumberHelper
 
-coin = ARGV[0] || 'nervos'
-response = HTTParty.get("https://api.f2pool.com/#{coin}/chabgood").parsed_response
-amount = response["value"]
-workers_online = response["worker_length_online"]
+class F2Pool
+  include HTTParty
+  include ActionView::Helpers::NumberHelper
 
-if workers_online > 0
-  amount = number_to_human(amount, precision: 4)
-  client = Twilio::REST::Client.new(ENV["ACCT_SID"], ENV["AUTH_TOKEN"])
-  client.messages.create(from: ENV["FROM"], to: ENV["TO"], body: "Total #{coin}: #{amount} \n Workers Online: #{workers_online}")
+  base_uri ENV["API"]
+  
+  attr_accessor :coin, :headers, :coin_amount, :workers_online, :client, :usd_amount
+  def initialize()
+    @coin = ARGV[0] || 'nervos'
+    @headers = { "X-CMC_PRO_API_KEY" => "#{ENV['API_KEY']}" }
+    @coin_amount = 0
+    @workers_online = 0
+    @usd_amount=0
+    initialize_twilio_info
+  end
+
+
+  def initialize_twilio_info
+    @account_sid = ENV["ACCT_SID"]
+    @auth_token = ENV["AUTH_TOKEN"]
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+  end
+
+  def run
+    get_f2poool_info
+    get_coinmarket_cap_data
+    send_sms
+  end
+
+
+  private
+
+  def get_f2poool_info
+    response = HTTParty.get("https://api.f2pool.com/#{self.coin}/chabgood").parsed_response
+    self.coin_amount = number_to_human(response["value"], precision: 4)
+    self.workers_online = response["worker_length_online"]
+  end
+
+  def get_coinmarket_cap_data
+    data = {'convert' => 'USD', 'amount' => "#{self.coin_amount}", 'symbol'=>"#{self.coin.upcase}"}
+    coin_data = HTTParty.get(ENV["API"], query: data, headers: self.headers).parsed_response
+    self.usd_amount = number_to_human(coin_data["data"]["quote"]["USD"]["price"],precision: 4)
+  end
+  
+  def send_sms
+    if self.workers_online > 0
+      client = Twilio::REST::Client.new(ENV["ACCT_SID"], ENV["AUTH_TOKEN"])
+      client.messages.create(from: ENV["FROM"], to: ENV["TO"], body: "Total #{self.coin}: #{self.coin_amount} \n Workers Online: #{workers_online} \n USD: #{self.usd_amount}")
+    end
+  end
 end
+
+f2pool = F2Pool.new
+f2pool.run
